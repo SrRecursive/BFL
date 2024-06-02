dc4755c307ef3c4c348cea8a584fd4ba70bcf130
SrVariable <rojohnibana@gmail.com>
2024-06-02 14:15:11 +0200

    Fix possible pointer reference loss

--- linked_list/bfl_ll_destroy_one.c
+++ linked_list/bfl_ll_destroy_one.c
@@ -6,7 +6,7 @@
 /*   By: ribana-b <ribana-b@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/02 12:39:30 by ribana-b          #+#    #+# Malaga      */
-/*   Updated: 2024/06/02 12:41:34 by ribana-b         ###   ########.com      */
+/*   Updated: 2024/06/02 14:08:25 by ribana-b         ###   ########.com      */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,12 @@
 
 void	bfl_ll_destroy_one(t_ll **linked_list)
 {
+	t_ll	*temp;
+
 	if (!linked_list || !(*linked_list))
 		return ;
+	temp = (*linked_list)->next;
 	bfl_ll_destroy_content(linked_list);
 	free(*linked_list);
+	*linked_list = temp;
 }
